5183c4fdc6e29e98b5481ee4f6c116c753bc6d66
Lucas <luc.coulson6@gmail.com>
2025-02-14 20:32:49 -0500

    switched numbers to rock paper and scissors

--- script.js
+++ script.js
@@ -1,12 +1,35 @@
-function getComputerChoice(max) {
+// Constants
+let humanScore = 0;
+let computerScore = 0;
+
+
+// functions
+function getComputerChoice(max) {       // random # from 0 - 2
     return Math.floor(Math.random() * max);
+
+}
+let max = getComputerChoice(3);
+
+if (max == 0) {
+    max = "Rock";
+} else if (max == 1) {
+    max = "Paper";
+} else {
+    max = "Scissors";
 }
 
-console.log(getComputerChoice(3));
+console.log(max);
 
-function getHumanChoice(choice) {
+function getHumanChoice(choice) { // player picks r, p, or s
     return choice;
 }
 
 let choice = prompt("Rock, Paper, or Scissors: ");
 console.log(getHumanChoice(choice));
+
+function playRound(humanChoice, computerChoice) {
+
+
+
+}
+
