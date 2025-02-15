73960a154081762f4d80f3442e03fb179bbab5d2
Lucas <luc.coulson6@gmail.com>
2025-02-14 20:05:31 -0500

    Added player input
    player can choose rock paper or scissors

--- script.js
+++ script.js
@@ -3,3 +3,10 @@ function getComputerChoice(max) {
 }
 
 console.log(getComputerChoice(3));
+
+function getHumanChoice(choice) {
+    return choice;
+}
+
+let choice = prompt("Rock, Paper, or Scissors: ");
+console.log(getHumanChoice(choice));
